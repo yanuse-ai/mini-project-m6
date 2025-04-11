3f4f5c458eda7e70fc36a03151726dfacf6c88a3
Yash <anuse.yp@gmail.com>
2025-04-11 16:33:25 +0000

    CD changes

--- employee_attrition_api/app/config.py
+++ employee_attrition_api/app/config.py
@@ -18,7 +18,7 @@ class Settings(BaseSettings):
         "https://localhost:8000",  # type: ignore
     ]
 
-    PROJECT_NAME: str = "Bike Share Count Prediction API"
+    PROJECT_NAME: str = "Employee attrition Prediction API"
 
     class Config:
         case_sensitive = True
